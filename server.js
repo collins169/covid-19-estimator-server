d5a4e6058f95d26b398a428fc1e7e24ad35f5316
Collins Onyetube <kosicollins169@gmail.com>
2020-04-09 06:22:53 +0000

    removed the logging to file

--- server.js
+++ server.js
@@ -26,32 +26,31 @@ app.use(bodyParser.xml({
 }));
 app.use(cors());
 
-app.use((req, res, next) => {
-  const start = new Date();
-  res.on('finish', function () {
-    const end = new Date();
-    const duration = end - start;
-  if(!fs.existsSync(path.join(__dirname, 'log.json'))){
-    fs.writeFileSync(path.join(__dirname, 'log.json'), JSON.stringify([]));
-  }
+// app.use((req, res, next) => {
+//   const start = new Date();
+//   res.on('finish', function () {
+//     const end = new Date();
+//     const duration = end - start;
+//   if(!fs.existsSync(path.join(__dirname, 'log.json'))){
+//     fs.writeFileSync(path.join(__dirname, 'log.json'), JSON.stringify([]));
+//   }
 
-  if(!fs.existsSync(path.join(__dirname, 'log.txt'))){
-    fs.writeFileSync(path.join(__dirname, 'log.txt'), "");
-  }
+//   if(!fs.existsSync(path.join(__dirname, 'log.txt'))){
+//     fs.writeFileSync(path.join(__dirname, 'log.txt'), "");
+//   }
 
-  let logFile = JSON.parse(fs.readFileSync(path.join(__dirname,'log.json'), 'utf8'));
-  const log = `${req.method}\t\t${req.path}\t\t${res.statusCode}\t\t${duration}ms`;
+//   let logFile = JSON.parse(fs.readFileSync(path.join(__dirname,'log.json'), 'utf8'));
+//   const log = `${req.method}\t\t${req.path}\t\t${res.statusCode}\t\t${duration}ms`;
   
-  let data = JSON.stringify({log}, null, 2);
-  logFile.push(JSON.parse(data));
-  if (res.statusCode === 200) {
-      fs.writeFileSync(path.join(__dirname, 'log.json'), JSON.stringify(logFile, null, 2));
-      fs.appendFileSync(path.join(__dirname, 'log.txt'), log + '\n', 'utf8');
-  }
-});
-
-  next();
-});
+//   let data = JSON.stringify({log}, null, 2);
+//   logFile.push(JSON.parse(data));
+//   if (res.statusCode === 200) {
+//       fs.writeFileSync(path.join(__dirname, 'log.json'), JSON.stringify(logFile, null, 2));
+//       fs.appendFileSync(path.join(__dirname, 'log.txt'), log + '\n', 'utf8');
+//   }
+// });
+//   next();
+// });
 
 app.use('/api/v1/on-covid-19', require('./routes/covidRouter'));
 
